dffb2fd958a72bab2f881c3d359bbf56f6497ecc
Mahdi <mahdimey17@gmail.com>
2023-12-06 23:44:30 +0330

    selected html elements from js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+// change h3 value when range input changes value
 const lengthInput = document.querySelector('#length')
 const lengthH3 = document.querySelector('.input-container h3')
 
 lengthInput.addEventListener('input', function() {
     lengthH3.textContent = this.value
 })
+
+// Get the UI elements
+const lowercaseCheckbox = document.getElementById('lowercase');
+const uppercaseCheckbox = document.getElementById('uppercase');
+const numbersCheckbox = document.getElementById('numbers');
+const symbolsCheckbox = document.getElementById('symbols');
+const lengthRangeInput = document.getElementById('length');
+const lengthLabel = document.querySelector('.length-label');
+const resaultContainer = document.querySelector('.resault-container');
+const resault = document.querySelector('.resault');
+const clipboardButton = document.getElementById('clippboard');
